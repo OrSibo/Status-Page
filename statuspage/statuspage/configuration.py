ac9f087c9d8488300458533de25ac51ea661e642
OrSibo <117725243+OrSibo@users.noreply.github.com>
2023-02-27 19:34:16 +0200

    Update configuration.py

--- statuspage/statuspage/configuration.py
+++ statuspage/statuspage/configuration.py
@@ -57,7 +57,7 @@ SITE_URL = ""
 # For optimal security, SECRET_KEY should be at least 50 characters in length and contain a mix of letters, numbers, and
 # symbols. Status-Page will not run without this defined. For more information, see
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-SECRET_KEY
-SECRET_KEY = statuspage/generate_secret_key.py
+SECRET_KEY = ''
 
 #
 # Optional Settings
