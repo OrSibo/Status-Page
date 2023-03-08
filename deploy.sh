a9020eae97d35aa4169aa1473ef3f0fab6f9540c
OrSibo <117725243+OrSibo@users.noreply.github.com>
2023-03-08 04:43:37 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ DOCKER_IMAGE=public.ecr.aws/f7b5d0k8/finalprojectorandhila:latest
 docker pull $DOCKER_IMAGE
 docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
 docker run -d -p 8080:8080 $DOCKER_IMAGE
+sh 'aws ecs update-service --service Final --cluster FinalCluster --force-new-deployment'
